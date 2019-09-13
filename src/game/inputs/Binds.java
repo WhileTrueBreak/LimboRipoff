a0e445b0f94436ea58f359d328c5c79e86876b11
Jeffrey <jeff.jc2003@gmail.com>
2019-09-13 10:17:43 +1000

    -Change jump to 'w'

--- src/game/inputs/Binds.java
+++ src/game/inputs/Binds.java
@@ -6,7 +6,7 @@ public class Binds {
 	
 	public static int MOVE_LEFT = KeyEvent.VK_A;
 	public static int MOVE_RIGHT = KeyEvent.VK_D;
-	public static int JUMP = KeyEvent.VK_SPACE;
+	public static int JUMP = KeyEvent.VK_W;
 	public static int SWITCH_LAYER_UP = KeyEvent.VK_Q;
 	public static int SWITCH_LAYER_DOWN = KeyEvent.VK_E;
 	public static int INTERACT = KeyEvent.VK_F;
